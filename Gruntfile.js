87444bba05f9f2548af3a1c7062550db5e182631
Andrey Grachev <andreysgra@gmail.com>
2018-02-14 00:02:40 +0300

    Обновление зависимостей. Корректировка конфигурации плагинов.

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,7 +82,13 @@ module.exports = function(grunt) {
           use: [
             require('imagemin-optipng')(),
             require('imagemin-svgo')({
-              plugins: [{removeTitle: true}]
+              plugins: [
+                {removeViewBox: false},
+                {removeTitle: true},
+                {cleanupNumericValues: {
+                  floatPrecision: 0
+                }
+              ]
             }),
             require('imagemin-jpegoptim')({
               max: 80,
@@ -131,7 +137,7 @@ module.exports = function(grunt) {
         options: {
           processors: [
             require('autoprefixer')(),
-            require('css-mqpacker')()
+            require('css-mqpacker')({sort: true})
           ]
         },
         src: 'build/css/*.css'
@@ -178,5 +184,7 @@ module.exports = function(grunt) {
 
   // Default tasks.
   grunt.registerTask('serve', ['browserSync', 'watch']);
-  grunt.registerTask('build', ['clean:build', 'copy:build', 'less', 'postcss', 'csso', 'concat', 'uglify', 'imagemin', 'svgstore', 'clean:icons']);
+  grunt.registerTask('style', ['less', 'postcss', 'csso']);
+  grunt.registerTask('images', ['imagemin', 'svgstore', 'clean:icons']);
+  grunt.registerTask('build', ['clean:build', 'copy:build', 'style', 'concat', 'uglify', 'images']);
 };
